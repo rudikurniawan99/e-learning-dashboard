ffb8dfab849118e9668d17131ecb6e35c4e889a1
saifoelloh <saifoelloh@gmail.com>
2022-03-03 21:07:41 +0700

    redux global resource setup

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 // reducer import
 import customizationReducer from './customizationReducer';
+import resources from './resources';
 
 // ==============================|| COMBINE REDUCER ||============================== //
 
 const reducer = combineReducers({
-    customization: customizationReducer
+    customization: customizationReducer,
+    resources
 });
 
 export default reducer;
---
+++ src/redux/reducers/resources.js
@@ -0,0 +1,41 @@
+import * as _ from 'lodash';
+import { DELETE_RESOURCE, GET_RESOURCE, RESORUCE_ADMIN, RESORUCE_QUESTION, RESORUCE_USER, UPDATE_RESOURCE } from 'redux/types';
+
+const initialState = {
+    [RESORUCE_ADMIN]: null,
+    [RESORUCE_USER]: null,
+    [RESORUCE_QUESTION]: null
+};
+
+export default (state = initialState, action) => {
+    const { resourceName, payload } = action;
+    switch (action.type) {
+        case GET_RESOURCE:
+            return state[resourceName];
+        case UPDATE_RESOURCE:
+            if (_.isArray(payload)) {
+                return {
+                    ...state,
+                    [resourceName]: {
+                        ...state[resourceName],
+                        ..._.keyBy(payload, 'id')
+                    }
+                };
+            }
+
+            return {
+                ...state,
+                [resourceName]: {
+                    ...state[resourceName],
+                    [payload.id]: payload
+                }
+            };
+        case DELETE_RESOURCE:
+            return {
+                ...state,
+                [resourceName]: _.omit(state[resourceName], payload.id)
+            };
+        default:
+            return state;
+    }
+};
