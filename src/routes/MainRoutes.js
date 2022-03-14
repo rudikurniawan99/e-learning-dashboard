3613086aa88ad42ff34b1c19a4ee8234b6a1604f
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-03-14 16:17:14 +0700

    update: delete unnecessary routing

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -8,11 +8,6 @@ import Loadable from 'ui-component/Loadable';
 const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
 
 // utilities routing
-const UtilsTypography = Loadable(lazy(() => import('views/utilities/Typography')));
-const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
-const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
-const UtilsMaterialIcons = Loadable(lazy(() => import('views/utilities/MaterialIcons')));
-const UtilsTablerIcons = Loadable(lazy(() => import('views/utilities/TablerIcons')));
 const AdminPage = Loadable(lazy(() => import('views/pages/admin')));
 const CreateAdminPage = Loadable(lazy(() => import('views/pages/admin/CreateAdmin')));
 const ProfilePage = Loadable(lazy(() => import('views/pages/profile')));
@@ -41,26 +36,6 @@ const MainRoutes = {
     {
       path: '/admin/create',
       element: <CreateAdminPage />
-    },
-    {
-      path: '/utils/util-typography',
-      element: <UtilsTypography />
-    },
-    {
-      path: '/utils/util-color',
-      element: <UtilsColor />
-    },
-    {
-      path: '/utils/util-shadow',
-      element: <UtilsShadow />
-    },
-    {
-      path: '/icons/tabler-icons',
-      element: <UtilsTablerIcons />
-    },
-    {
-      path: '/icons/material-icons',
-      element: <UtilsMaterialIcons />
     },
     {
       path: '/sample-page',
